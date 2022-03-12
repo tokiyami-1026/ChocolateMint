91c27ffdbbff3f7a2465fe1fad1e49ebc17e0cd5
takumyon <takumyon5145563@gmail.com>
2022-03-12 12:24:08 +0900

    ゲームシーン実装

---
+++ Assets/Plugins/ChocolateMint/Scripts/Scene/GameSceneController.cs
@@ -0,0 +1,44 @@
+﻿using ChocolateMint.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScene = UnityEngine.SceneManagement.Scene;
+
+namespace ChocolateMint.Scene
+{
+    public abstract class GameSceneController<TModel, TView> : DisplayContentController<TModel, TView>, IDisplayContentLoadingCallbackReceiverInternal<UnityScene>
+        where TModel : DisplayContentModel, new()
+        where TView : DisplayContentView
+    {
+        /// <summary>
+        /// ロードが完了した
+        /// </summary>
+        /// <param name="loadResult">ロードが完了したUnityのシーン</param>
+        void IDisplayContentLoadingCallbackReceiverInternal<UnityScene>.OnLoadedInternal(UnityScene loadedScene)
+        {
+            Initialize();
+            Model.Initialize();
+
+            var rootObjects = loadedScene.GetRootGameObjects();
+            foreach (var root in rootObjects)
+            {
+                view = root.GetComponentInChildren<TView>(includeInactive:true);
+                if (view != null)
+                {
+                    break;
+                }
+            }
+
+            if (view != null)
+            {
+                view.InitializeInternal(MessageBroker);
+                view.Initialize();
+            }
+            else
+            {
+                throw new Exception("シーン内のViewの取得に失敗しました。");
+            }
+        }
+    }
+}
---
+++ Assets/SandBox/TestScripts/SandBoxSceneView.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChocolateMint.Common;
+
+public class SandBoxSceneView : DisplayContentView
+{
+    
+}
